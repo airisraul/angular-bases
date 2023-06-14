d882268cd4920c96df18b55efa73b1a6d33f33a5
Raul Vargas <r.vargas@tesipro.com>
2023-06-14 20:10:56 +0200

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera APP Angular';
+  public counter: number = 10;
+
+  increaseBy( value: number):void {
+      this.counter+=value;
+  }
+
+  resetCounter() : void{
+    this.counter=10;
+  }
+
 }
